33c342bc2e4db2307e450393e07004559b78dabc
lobaojoao <joao.lobao@southsystem.com.br>
2023-05-29 23:24:36 -0300

    feat: added delete and add in list

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, SafeAreaView, Button, FlatList} from 'react-native';
 
-export default function App() {
+
+
+
+export default App = () => {
+  const [arrayToDo, setArrayToDo] = useState(["Comprar caderno", "Comprar lapis", "Ir ao mercado"])
+  
+  const handleAdd = () => {
+    setArrayToDo([...arrayToDo, "Comer maçã"])
+  }
+
+  const handleRemove = (index) => {            
+    const removeItem = arrayToDo.filter(item => item !== arrayToDo[index])
+    setArrayToDo(removeItem)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <FlatList 
+          data={arrayToDo}
+          keyExtractor={item => item}
+          renderItem={({ item, index }) => (
+            <View style={{flexDirection: 'row'}}>
+              <Text style={{fontSize: 40}}>{item}</Text>
+              <Button 
+                onPress={() => handleRemove(index)}
+                title="Deletar"
+                color="red"
+              />
+            </View>
+            
+          )}
+     
+        />
+          <Button 
+            onPress={handleAdd}
+            title="Adicionar"
+            color="red"
+          />
+       
+ 
+    </SafeAreaView>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
